faa57235109363709f899db7435996c20a6056bc
jominjos <sjominjos@gmail.com>
2024-06-22 14:22:07 +0530

    style and resp changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import CallToAction from "../components/CallToAction";
 import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
 import Carousel from "../components/Carousel";
@@ -8,62 +7,57 @@ import Cookies from "js-cookie";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
-  const slides = [
-    //"https://live.staticflickr.com/65535/52982630415_6af366993c_h.jpg",
-    "https://firebasestorage.googleapis.com/v0/b/tvkblog-4b39f.appspot.com/o/speech.jpg?alt=media&token=d54453e6-e2ec-4038-aafd-103b3ff4c50c",
-    "https://firebasestorage.googleapis.com/v0/b/tvkblog-4b39f.appspot.com/o/patti.jpg?alt=media&token=3c54c481-7674-41f8-9457-013bd9052925",
-    "https://firebasestorage.googleapis.com/v0/b/tvkblog-4b39f.appspot.com/o/tholar.jpg?alt=media&token=b95ddeb5-0125-4f42-860d-0446839d36e6",
-    // "https://live.staticflickr.com/65535/52982259731_5c36b72b09_b.jpg",
-    //"https://live.staticflickr.com/65535/52980540197_be36a66b41_b.jpg",
-  ];
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Determine if the screen is mobile on component mount
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 640); // Example breakpoint for mobile
+    };
+    handleResize(); // Initial check
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const slides = isMobile
+    ? [
+        "https://firebasestorage.googleapis.com/v0/b/tvkblog-4b39f.appspot.com/o/speech.jpg?alt=media&token=d54453e6-e2ec-4038-aafd-103b3ff4c50c",
+        "https://firebasestorage.googleapis.com/v0/b/tvkblog-4b39f.appspot.com/o/patti.jpg?alt=media&token=3c54c481-7674-41f8-9457-013bd9052925",
+        "https://firebasestorage.googleapis.com/v0/b/tvkblog-4b39f.appspot.com/o/tholar.jpg?alt=media&token=b95ddeb5-0125-4f42-860d-0446839d36e6",
+      ]
+    : [
+        "https://live.staticflickr.com/65535/52982259731_5c36b72b09_b.jpg",
+        "https://live.staticflickr.com/65535/52980540197_be36a66b41_b.jpg",
+        "https://live.staticflickr.com/65535/52982630415_6af366993c_h.jpg",
+        // Additional desktop slides if needed
+      ];
+
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch(`${API_BASE_URL}/api/post/getPosts`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-          jwt_token: ` ${Cookies.get("token")}`,
-          // Other headers if needed
-        },
-      });
-      const data = await res.json();
-      //
-
-      //
-      console.log("getting posts");
-
-      setPosts(data.posts);
-      // setPosts([
-      //   {
-      //     title: "Blog name",
-      //     category: "article category",
-      //     image:
-      //       "https://live.staticflickr.com/65535/52982259731_5c36b72b09_b.jpg",
-      //     slug: "as",
-      //   },
-      // ]);
+      try {
+        const res = await fetch(`${API_BASE_URL}/api/post/getPosts`, {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+            jwt_token: ` ${Cookies.get("token")}`,
+          },
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setPosts(data.posts);
+        } else {
+          console.error("Failed to fetch posts");
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
     };
     fetchPosts();
   }, []);
+
   return (
     <div>
-      {/* <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
-        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
-        <p className="text-gray-500 text-xs sm:text-sm">
-          Here you'll find a variety of articles and tutorials on topics such as
-          web development, software engineering, and programming languages.
-        </p>
-        <Link
-          to="/search"
-          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
-        >
-          View all posts
-        </Link>
-      </div> */}
-      {/* <div className="p-3 bg-amber-100 dark:bg-slate-700">
-        <CallToAction />
-      </div> */}
       <div>
         <Carousel>
           {slides.map((s) => (
@@ -75,7 +69,7 @@ export default function Home() {
         {posts && posts.length > 0 && (
           <div className="flex flex-col gap-6">
             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-            <div className="flex flex-wrap gap-4">
+            <div className="flex flex-wrap gap-4 justify-around">
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
