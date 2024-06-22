905f77b31cfd7db47939edc9d4d16fb82f0eb42c
jominjos <sjominjos@gmail.com>
2024-06-22 13:22:53 +0530

    category fix

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -155,9 +155,9 @@ export default function Search() {
               id="category"
             >
               <option value="uncategorized">Uncategorized</option>
-              <option value="reactjs">React.js</option>
-              <option value="nextjs">Next.js</option>
-              <option value="javascript">JavaScript</option>
+              <option value="Service">Service</option>
+              <option value="Election">Election</option>
+              {/* <option value="javascript">JavaScript</option> */}
             </Select>
           </div>
           <Button type="submit" outline gradientDuoTone="purpleToPink">
